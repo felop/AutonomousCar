66ed061b725c9ef20b17bb8c7e4b2b69908c62af
Félox <contact@felixlop.com>
2021-08-23 13:43:47 +0200

    Create test_iso_picam.py

---
+++ imageManagement/test_iso_picam.py
@@ -0,0 +1,21 @@
+import os, io, picamera, time, cv2
+
+camera = picamera.PiCamera()
+u = int(input("u = "))
+camera.resolution = (80,64)
+camera.iso = 100
+camera.framerate = 80
+camera.start_preview()
+isos = []
+for i in range(10):
+    iso = 100*(i+1)
+    u += 1
+    time.sleep(0.1)
+    print("pic nb "+str(u)+" , iso : "+str(iso))
+    camera.iso = iso
+    title = "iso/"+str(iso)+"_"+str(u)+".png"
+    camera.capture(title , use_video_port=True)
+    img = cv2.imread(title , cv2.IMREAD_GRAYSCALE)
+    img = cv2.resize(img,(1,1))
+    isos.append(img[0][0])
+print(iso)
